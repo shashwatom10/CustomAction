6ba77f94d242e9a2f0d4f5dcec0a00bdf03bf2d2
shashwat <shashwatom10@gmail.com>
2024-10-01 10:11:00 +0530

    customAction

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const core = require('@actions/core');
+ 
+try {
+    const person = process.env.INPUT_PERSON;  // Access the input
+    console.log(`Hello ${person} from custom js actions`);  // Use backticks for string interpolation
+} catch (error) {
+    core.setFailed(`Action failed: ${error}`);
+}
+ 
